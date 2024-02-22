d41b305743fe32eb712c42a5fb89ff2cac0af8ce
leo <nascimentoleo899@gmail.com>
2024-02-21 21:52:59 -0300

    implement getters in user entity

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -12,10 +12,33 @@ describe('UserEntity Uint Test', () => {
     }
     sut = new UserEntity(props)
   })
-  it('Constructor method', () => {
+  it('Should contruct the user entity', () => {
     expect(sut.props.name).toEqual(props.name)
     expect(sut.props.email).toEqual(props.email)
     expect(sut.props.password).toEqual(props.password)
+    expect(sut.props.cratedAt).toBeInstanceOf(Date)
+  })
+
+  it('Should return name', () => {
+    expect(sut.props.name).toBeDefined()
+    expect(sut.props.name).toEqual(props.name)
+    expect(typeof sut.props.name).toBe('string')
+  })
+
+  it('Should return email', () => {
+    expect(sut.props.email).toBeDefined()
+    expect(sut.props.email).toEqual(props.email)
+    expect(typeof sut.props.email).toBe('string')
+  })
+
+  it('Should return password', () => {
+    expect(sut.props.password).toBeDefined()
+    expect(sut.props.password).toEqual(props.password)
+    expect(typeof sut.props.password).toBe('string')
+  })
+
+  it('Should return cratedAt', () => {
+    expect(sut.props.cratedAt).toBeDefined()
     expect(sut.props.cratedAt).toBeInstanceOf(Date)
   })
 })
--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -9,4 +9,20 @@ export class UserEntity {
   constructor(readonly props: UserProps) {
     this.props.cratedAt = this.props.cratedAt ?? new Date()
   }
+
+  get name(): string {
+    return this.props.name
+  }
+
+  get email(): string {
+    return this.props.email
+  }
+
+  get password(): string {
+    return this.props.password
+  }
+
+  get cratedAt(): Date {
+    return this.props.cratedAt
+  }
 }
