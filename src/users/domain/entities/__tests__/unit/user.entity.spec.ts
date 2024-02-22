eb83a09115bf3da190cb0915279cd02e4912182f
leo <nascimentoleo899@gmail.com>
2024-02-21 22:00:22 -0300

    adding UserDataBuilder helper

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,15 +1,11 @@
-import { faker } from '@faker-js/faker'
 import { UserEntity, UserProps } from '../../user.entity'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
 
 describe('UserEntity Uint Test', () => {
   let props: UserProps
   let sut: UserEntity
   beforeEach(() => {
-    props = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    }
+    props = UserDataBuilder({})
     sut = new UserEntity(props)
   })
   it('Should contruct the user entity', () => {
