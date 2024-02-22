cf93dc0e30233eb54ba9134d1c959ab12d5fe0dc
leo <nascimentoleo899@gmail.com>
2024-02-21 21:45:13 -0300

    create user entity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class UserEntity {}
+export type UserProps = {
+  name: string
+  email: string
+  password: string
+  cratedAt?: Date
+}
+
+export class UserEntity {
+  constructor(readonly props: UserProps) {
+    this.props.cratedAt = this.props.cratedAt ?? new Date()
+  }
+}
