0fd768a31909a56ef117e3c69b184a6e62a704c6
leo <nascimentoleo899@gmail.com>
2024-02-24 16:24:48 -0300

    add unit tests for ClassValidatorFields

---
+++ src/shared/domain/validators/__tests__/unit/class-validator-fields.spec.ts
@@ -0,0 +1,26 @@
+import { ClassValidatorFields } from '../../class-validator-fields'
+import * as classValidator from 'class-validator'
+
+class StubClassValidatorFields extends ClassValidatorFields<{
+  field: string
+}> {}
+
+describe('ClassValidatorFields Unit Tests', () => {
+  it('Should initialize errors with null', () => {
+    const sut = new StubClassValidatorFields()
+    expect(sut.errors).toBeNull()
+    expect(sut.validateData).toBeNull()
+  })
+
+  it('Should return true if validate method returns no errors', () => {
+    const spyValidateSync = jest.spyOn(classValidator, 'validateSync')
+    spyValidateSync.mockReturnValue([
+      { property: 'field', constraints: { isRequired: 'test error' } },
+    ])
+    const sut = new StubClassValidatorFields()
+    expect(sut.validate(null)).toBeFalsy()
+    expect(spyValidateSync).toHaveBeenCalled()
+    expect(sut.validateData).toBeNull()
+    expect(sut.errors).toEqual({ field: ['test error'] })
+  })
+})
