437c29e16ec7d1760c5c737fde911f6767662a20
leo <nascimentoleo899@gmail.com>
2024-03-17 20:31:33 -0300

    Add tests for deleting entities in InMemoryRepository

--- src/shared/domain/repository/__tests__/unit/in-memory.repository.spec.ts
+++ src/shared/domain/repository/__tests__/unit/in-memory.repository.spec.ts
@@ -1,6 +1,7 @@
 import { Entity } from '@/shared/domain/entity/entity'
 import { InMemoryRepository } from '../../in-memory.repository'
 import { NotFoundError } from '@/shared/domain/errors/not-found-error'
+import e from 'express'
 
 type StubEntityProps = {
   name: string
@@ -21,13 +22,14 @@ describe('InMemoryRepository Unit Tests', () => {
     const entity = new StubEntity({ name: 'any_name', price: 10 })
     await sut.insert(entity)
     expect(entity.toJSON()).toStrictEqual(sut.items[0].toJSON())
+    expect(sut.items).toHaveLength(1)
   })
 
   it('should find entity by id', async () => {
     const entity = new StubEntity({ name: 'test name', price: 50 })
     await sut.insert(entity)
-    const result = await sut.findById(entity.id)
-    expect(result).toStrictEqual(entity)
+    const result = await sut.findById(entity._id)
+    expect(result).toEqual(entity)
   })
 
   it('should throw if entity not found', async () => {
@@ -58,6 +60,19 @@ describe('InMemoryRepository Unit Tests', () => {
   it('should throw if entity not found when update entity', async () => {
     const entity = new StubEntity({ name: 'test name', price: 50 })
     await expect(sut.update(entity)).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    )
+  })
+
+  it('should delete entity', async () => {
+    const entity = new StubEntity({ name: 'test name 1', price: 25 })
+    await sut.insert(entity)
+    await sut.delete(entity._id)
+    expect(sut.items).toHaveLength(0)
+  })
+
+  it('should throw if entity not found when delete entity', async () => {
+    await expect(sut.delete('fakeIdToDelete')).rejects.toThrow(
       new NotFoundError('Entity not found'),
     )
   })
