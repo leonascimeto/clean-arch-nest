17a8652e182ea2211ffb77f1a8f932ca5f2e561b
leo <nascimentoleo899@gmail.com>
2024-03-24 13:17:23 -0300

    add UserInMemory implementation

---
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -0,0 +1,21 @@
+import { NotFoundError } from '@/shared/domain/errors/not-found-error'
+import { InMemoryRepository } from '@/shared/domain/repository/in-memory.repository'
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserRepository } from '@/users/domain/repositories/user.repository'
+
+export class UserInMemoryRepository
+  extends InMemoryRepository<UserEntity>
+  implements UserRepository
+{
+  async findByEmail(email: string): Promise<UserEntity> {
+    const entity = this.items.find(item => item.email === email)
+    if (!entity)
+      throw new NotFoundError(`Entity not found using email ${email}`)
+    return entity
+  }
+
+  async emailExists(email: string): Promise<void> {
+    const entity = this.items.find(item => item.email === email)
+    if (entity) throw new Error(`Email ${email} already exists`)
+  }
+}
